5a55be84b10ec9828e9799fa03f5be78f5105304
msl9204 <52311764+msl9204@users.noreply.github.com>
2019-09-26 15:46:04 +0900

    Create khan2-3.sql

---
+++ khan2-3.sql
@@ -0,0 +1,51 @@
+DROP TABLE books;
+
+CREATE TABLE books (
+    id INTEGER PRIMARY KEY,
+    author VARCHAR2(50),
+    title VARCHAR2(50),
+    words INTEGER);
+
+DROP SEQUENCE seq_book;
+
+CREATE SEQUENCE seq_book start with 1 increment by 1 minvalue 0 nocache;
+  
+    
+INSERT INTO books (id, author, title, words)
+    VALUES (seq_book.NEXTVAL, 'J.K. Rowling', 'Harry Potter and the Philosopher''s Stone', 79944);
+INSERT INTO books (id, author, title, words)
+    VALUES (seq_book.NEXTVAL, 'J.K. Rowling', 'Harry Potter and the Chamber of Secrets', 85141);
+INSERT INTO books (id, author, title, words)
+    VALUES (seq_book.NEXTVAL, 'J.K. Rowling', 'Harry Potter and the Prisoner of Azkaban', 107253);
+INSERT INTO books (id, author, title, words)
+    VALUES (seq_book.NEXTVAL, 'J.K. Rowling', 'Harry Potter and the Goblet of Fire', 190637);
+INSERT INTO books (id, author, title, words)
+    VALUES (seq_book.NEXTVAL, 'J.K. Rowling', 'Harry Potter and the Order of the Phoenix', 257045);
+INSERT INTO books (id, author, title, words)
+    VALUES (seq_book.NEXTVAL, 'J.K. Rowling', 'Harry Potter and the Half-Blood Prince', 168923);
+INSERT INTO books (id, author, title, words)
+    VALUES (seq_book.NEXTVAL, 'J.K. Rowling', 'Harry Potter and the Deathly Hallows', 197651);
+
+INSERT INTO books (id, author, title, words)
+    VALUES (seq_book.NEXTVAL, 'Stephenie Meyer', 'Twilight', 118501);
+INSERT INTO books (id, author, title, words)
+    VALUES (seq_book.NEXTVAL, 'Stephenie Meyer', 'New Moon', 132807);
+INSERT INTO books (id, author, title, words)
+    VALUES (seq_book.NEXTVAL, 'Stephenie Meyer', 'Eclipse', 147930);
+INSERT INTO books (id, author, title, words)
+    VALUES (seq_book.NEXTVAL, 'Stephenie Meyer', 'Breaking Dawn', 192196);
+    
+INSERT INTO books (id, author, title, words)
+    VALUES (seq_book.NEXTVAL, 'J.R.R. Tolkien', 'The Hobbit', 95022);
+INSERT INTO books (id, author, title, words)
+    VALUES (seq_book.NEXTVAL, 'J.R.R. Tolkien', 'Fellowship of the Ring', 177227);
+INSERT INTO books (id, author, title, words)
+    VALUES (seq_book.NEXTVAL, 'J.R.R. Tolkien', 'Two Towers', 143436);
+INSERT INTO books (id, author, title, words)
+    VALUES (seq_book.NEXTVAL, 'J.R.R. Tolkien', 'Return of the King', 134462);
+    
+    
+SELECT author, SUM(words) AS total_words FROM books GROUP BY author HAVING total_words >= 1000000;
+
+SELECT author, AVG(words) AS avg_words FROM books GROUP BY author HAVING avg_words >= 150000;
+ 
