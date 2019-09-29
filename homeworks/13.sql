77b2b62ed5f02b85f164985e03d5eda3324a55a1
msl9204 <52311764+msl9204@users.noreply.github.com>
2019-09-30 07:37:50 +0900

    Update 13.sql

--- homeworks/13.sql
+++ homeworks/13.sql
@@ -1,14 +1,16 @@
+CREATE SEQUENCE id_seq start with 1 increment by 1 minvalue 0 nocache;
+
 CREATE TABLE clothes (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INTEGER PRIMARY KEY,
     type TEXT,
     design TEXT);
     
-INSERT INTO clothes (type, design)
-    VALUES ("dress", "pink polka dots");
-INSERT INTO clothes (type, design)
-    VALUES ("pants", "rainbow tie-dye");
-INSERT INTO clothes (type, design)
-    VALUES ("blazer", "black sequin");
+INSERT INTO clothes (id, type, design)
+    VALUES (id_seq.NEXTVAL, 'dress', 'pink polka dots');
+INSERT INTO clothes (id, type, design)
+    VALUES (id_seq.NEXTVAL, 'pants', 'rainbow tie-dye');
+INSERT INTO clothes (id, type, design)
+    VALUES (id_seq.NEXTVAL, 'blazer', 'black sequin');
 
 ALTER TABLE clothes add price INTEGER;
 
@@ -20,6 +22,9 @@ UPDATE clothes SET price = 30 WHERE id=3;
 
 SELECT * FROM clothes;
 
-INSERT INTO clothes (type, design, price) VALUES ("shoes", "high-hill", 40);
+INSERT INTO clothes (type, design, price) VALUES ('shoes', 'high-hill', 40);
 
 SELECT * FROM clothes;
+
+DROP TABLE clothes;
+DROP SEQUENCE id_seq;
