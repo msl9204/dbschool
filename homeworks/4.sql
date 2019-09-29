07419d45d0fac5bee3e531d8128dea28d03aa74d
msl9204 <52311764+msl9204@users.noreply.github.com>
2019-09-30 06:56:53 +0900

    Update 4.sql

--- homeworks/4.sql
+++ homeworks/4.sql
@@ -1,29 +1,34 @@
+CREATE SEQUENCE tmp_id START WITH 0 INCREMENT BY 1 MINVALUE 0 MAXVALUE 100 CYCLE NOCACHE;
+
+
 CREATE TABLE songs (
     id INTEGER PRIMARY KEY,
-    title TEXT,
-    artist TEXT,
-    mood TEXT,
+    title VARCHAR2(50),
+    artist VARCHAR2(50),
+    mood VARCHAR2(50),
     duration INTEGER,
     released INTEGER);
     
-INSERT INTO songs (title, artist, mood, duration, released)
-    VALUES ("Bohemian Rhapsody", "Queen", "epic", 60, 1975);
-INSERT INTO songs (title, artist, mood, duration, released)
-    VALUES ("Let it go", "Idina Menzel", "epic", 227, 2013);
-INSERT INTO songs (title, artist, mood, duration, released)
-    VALUES ("I will survive", "Gloria Gaynor", "epic", 198, 1978);
-INSERT INTO songs (title, artist, mood, duration, released)
-    VALUES ("Twist and Shout", "The Beatles", "happy", 152, 1963);
-INSERT INTO songs (title, artist, mood, duration, released)
-    VALUES ("La Bamba", "Ritchie Valens", "happy", 166, 1958);
-INSERT INTO songs (title, artist, mood, duration, released)
-    VALUES ("I will always love you", "Whitney Houston", "epic", 273, 1992);
-INSERT INTO songs (title, artist, mood, duration, released)
-    VALUES ("Sweet Caroline", "Neil Diamond", "happy", 201, 1969);
-INSERT INTO songs (title, artist, mood, duration, released)
-    VALUES ("Call me maybe", "Carly Rae Jepsen", "happy", 193, 2011);
+INSERT INTO songs (id, title, artist, mood, duration, released)
+    VALUES (tmp_id.NEXTVAL, 'Bohemian Rhapsody', 'Queen', 'epic', 60, 1975);
+INSERT INTO songs (id, title, artist, mood, duration, released)
+    VALUES (tmp_id.NEXTVAL, 'Let it go', 'Idina Menzel', 'epic', 227, 2013);
+INSERT INTO songs (id, title, artist, mood, duration, released)
+    VALUES (tmp_id.NEXTVAL, 'I will survive', 'Gloria Gaynor', 'epic', 198, 1978);
+INSERT INTO songs (id, title, artist, mood, duration, released)
+    VALUES (tmp_id.NEXTVAL, 'Twist and Shout', 'The Beatles', 'happy', 152, 1963);
+INSERT INTO songs (id, title, artist, mood, duration, released)
+    VALUES (tmp_id.NEXTVAL, 'La Bamb', 'Ritchie Valens', 'happy', 166, 1958);
+INSERT INTO songs (id, title, artist, mood, duration, released)
+    VALUES (tmp_id.NEXTVAL, 'I will always love you', 'Whitney Houston', 'epic', 273, 1992);
+INSERT INTO songs (id, title, artist, mood, duration, released)
+    VALUES (tmp_id.NEXTVAL, 'Sweet Caroline', 'Neil Diamond', 'happy', 201, 1969);
+INSERT INTO songs (id, title, artist, mood, duration, released)
+    VALUES (tmp_id.NEXTVAL, 'Call me maybe', 'Carly Rae Jepsen', 'happy', 193, 2011);
     
 SELECT title FROM songs;
-SELECT title FROM songs WHERE (mood = 'epic') OR (released >= 1990);
+SELECT title FROM songs WHERE (mood = epic') OR (released >= 1990);
 SELECT title FROM songs WHERE (mood = 'epic') AND (released >= 1990) AND (duration <= 240);
 
+DROP SEQUENCE tmp_id;
+DROP TABLE songs;
