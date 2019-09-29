cb480823e751235eb1992efca57778fc27919a8d
msl9204 <52311764+msl9204@users.noreply.github.com>
2019-09-30 07:35:09 +0900

    Update 12.sql

--- homeworks/12.sql
+++ homeworks/12.sql
@@ -1,26 +1,28 @@
+CREATE SEQUENCE id_seq start with 1 increment by 1 minvalue 0 nocache;
+
 CREATE table documents (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT,
-    content TEXT,
-    author TEXT);
+    id INTEGER PRIMARY KEY,
+    title VARCHAR2(70),
+    content VARCHAR2(70),
+    author VARCHAR2(70));
     
-INSERT INTO documents (author, title, content)
-    VALUES ("Puff T.M. Dragon", "Fancy Stuff", "Ceiling wax, dragon wings, etc.");
-INSERT INTO documents (author, title, content)
-    VALUES ("Puff T.M. Dragon", "Living Things", "They're located in the left ear, you know.");
-INSERT INTO documents (author, title, content)
-    VALUES ("Jackie Paper", "Pirate Recipes", "Cherry pie, apple pie, blueberry pie.");
-INSERT INTO documents (author, title, content)
-    VALUES ("Jackie Paper", "Boat Supplies", "Rudder - guitar. Main mast - bed post.");
-INSERT INTO documents (author, title, content)
-    VALUES ("Jackie Paper", "Things I'm Afraid Of", "Talking to my parents, the sea, giant pirates, heights.");
+INSERT INTO documents (id, author, title, content)
+    VALUES (id_seq.NEXTVAL, 'Puff T.M. Dragon', 'Fancy Stuff', 'Ceiling wax, dragon wings, etc.');
+INSERT INTO documents (id, author, title, content)
+    VALUES (id_seq.NEXTVAL, 'Puff T.M. Dragon', 'Living Things', 'They''re located in the left ear, you know.');
+INSERT INTO documents (id, author, title, content)
+    VALUES (id_seq.NEXTVAL, 'Jackie Paper', 'Pirate Recipes', 'Cherry pie, apple pie, blueberry pie.');
+INSERT INTO documents (id, author, title, content)
+    VALUES (id_seq.NEXTVAL, 'Jackie Paper', 'Boat Supplies', 'Rudder - guitar. Main mast - bed post.');
+INSERT INTO documents (id, author, title, content)
+    VALUES (id_seq.NEXTVAL, 'Jackie Paper', 'Things I''m Afraid Of', 'Talking to my parents, the sea, giant pirates, heights.');
 
 SELECT * FROM documents;
 
-UPDATE documents SET author = "Jackie Draper" WHERE author LIKE "Jackie Paper";
+UPDATE documents SET author = 'Jackie Draper' WHERE author LIKE 'Jackie Paper';
 
 SELECT * FROM documents;
 
-DELETE FROM documents WHERE title LIKE "Things I'm Afraid Of";
+DELETE FROM documents WHERE title LIKE 'Things I''m Afraid Of';
 
 SELECT * FROM documents;
