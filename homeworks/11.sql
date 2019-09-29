0f7a5d8c0b52d58594dfa848ff520fcf87fa5977
msl9204 <52311764+msl9204@users.noreply.github.com>
2019-09-30 07:32:02 +0900

    Update 11.sql

--- homeworks/11.sql
+++ homeworks/11.sql
@@ -1,39 +1,43 @@
+CREATE SEQUENCE id_seq start with 1 increment by 1 minvalue 0 nocache;
+CREATE SEQUENCE id_seq2 start with 1 increment by 1 minvalue 0 nocache;
+CREATE SEQUENCE id_seq3 start with 1 increment by 1 minvalue 0 nocache;
+
 CREATE TABLE persons (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    fullname TEXT,
+    id INTEGER PRIMARY KEY,
+    fullname VARCHAR(50),
     age INTEGER);
     
-INSERT INTO persons (fullname, age) VALUES ("Bobby McBobbyFace", "12");
-INSERT INTO persons (fullname, age) VALUES ("Lucy BoBucie", "25");
-INSERT INTO persons (fullname, age) VALUES ("Banana FoFanna", "14");
-INSERT INTO persons (fullname, age) VALUES ("Shish Kabob", "20");
-INSERT INTO persons (fullname, age) VALUES ("Fluffy Sparkles", "8");
+INSERT INTO persons (id, fullname, age) VALUES (id_seq.NEXTVAL, 'Bobby McBobbyFace', '12');
+INSERT INTO persons (id, fullname, age) VALUES (id_seq.NEXTVAL, 'Lucy BoBucie', '25');
+INSERT INTO persons (id, fullname, age) VALUES (id_seq.NEXTVAL, 'Banana FoFanna', '14');
+INSERT INTO persons (id, fullname, age) VALUES (id_seq.NEXTVAL, 'Shish Kabob', '20');
+INSERT INTO persons (id, fullname, age) VALUES (id_seq.NEXTVAL, 'Fluffy Sparkles', '8');
 
 CREATE table hobbies (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INTEGER PRIMARY KEY,
     person_id INTEGER,
-    name TEXT);
+    name VARCHAR(50));
     
-INSERT INTO hobbies (person_id, name) VALUES (1, "drawing");
-INSERT INTO hobbies (person_id, name) VALUES (1, "coding");
-INSERT INTO hobbies (person_id, name) VALUES (2, "dancing");
-INSERT INTO hobbies (person_id, name) VALUES (2, "coding");
-INSERT INTO hobbies (person_id, name) VALUES (3, "skating");
-INSERT INTO hobbies (person_id, name) VALUES (3, "rowing");
-INSERT INTO hobbies (person_id, name) VALUES (3, "drawing");
-INSERT INTO hobbies (person_id, name) VALUES (4, "coding");
-INSERT INTO hobbies (person_id, name) VALUES (4, "dilly-dallying");
-INSERT INTO hobbies (person_id, name) VALUES (4, "meowing");
+INSERT INTO hobbies (id, person_id, name) VALUES (id_seq2.NEXTVAL, 1, 'drawing');
+INSERT INTO hobbies (id, person_id, name) VALUES (id_seq2.NEXTVAL, 1, 'coding');
+INSERT INTO hobbies (id, person_id, name) VALUES (id_seq2.NEXTVAL, 2, 'dancing');
+INSERT INTO hobbies (id, person_id, name) VALUES (id_seq2.NEXTVAL, 2, 'coding');
+INSERT INTO hobbies (id, person_id, name) VALUES (id_seq2.NEXTVAL, 3, 'skating');
+INSERT INTO hobbies (id, person_id, name) VALUES (id_seq2.NEXTVAL, 3, 'rowing');
+INSERT INTO hobbies (id, person_id, name) VALUES (id_seq2.NEXTVAL, 3, 'drawing');
+INSERT INTO hobbies (id, person_id, name) VALUES (id_seq2.NEXTVAL, 4, 'coding');
+INSERT INTO hobbies (id, person_id, name) VALUES (id_seq2.NEXTVAL, 4, 'dilly-dallying');
+INSERT INTO hobbies (id, person_id, name) VALUES (id_seq2.NEXTVAL, 4, 'meowing');
 
 CREATE table friends (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INTEGER PRIMARY KEY,
     person1_id INTEGER,
     person2_id INTEGER);
 
-INSERT INTO friends (person1_id, person2_id)
-    VALUES (1, 4);
-INSERT INTO friends (person1_id, person2_id)
-    VALUES (2, 3);
+INSERT INTO friends (id, person1_id, person2_id)
+    VALUES (id_seq3.NEXTVAL, 1, 4);
+INSERT INTO friends (id, person1_id, person2_id)
+    VALUES (id_seq3.NEXTVAL, 2, 3);
     
 SELECT persons.fullname, hobbies.name FROM persons
     JOIN hobbies
