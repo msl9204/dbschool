4203ccd93033aa47c94607811469e3d67c3ed601
msl9204 <52311764+msl9204@users.noreply.github.com>
2019-09-30 08:14:28 +0900

    Update 9.sql

--- homeworks/9.sql
+++ homeworks/9.sql
@@ -32,8 +32,8 @@ SELECT customers.name, customers.email, SUM(orders.price) AS SUM FROM
     (SELECT customers.name, customers.email, SUM(orders.price) AS SUM FROM customers
     LEFT OUTER JOIN orders
     ON customers.id = orders.customer_id
-    GROUP BY customers.name
-    ORDER BY SUM DESC);
+    GROUP BY customers.name)
+    ORDER BY SUM DESC;
                    
 DROP SEQUENCE id_seq;
 DROP SEQUENCE id_seq2;
