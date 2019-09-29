7902ffda81164ccc0501d9b2fb5ca71753344282
msl9204 <52311764+msl9204@users.noreply.github.com>
2019-09-30 07:20:04 +0900

    Update 9.sql

--- homeworks/9.sql
+++ homeworks/9.sql
@@ -1,24 +1,26 @@
+CREATE SEQUENCE id_seq start with 1 increment by 1 minvalue 0 nocache;
+
 CREATE TABLE customers (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    email TEXT);
+    id INTEGER PRIMARY KEY,
+    name VARCHAR2(50),
+    email VARCHAR2(50));
     
-INSERT INTO customers (name, email) VALUES ("Doctor Who", "doctorwho@timelords.com");
-INSERT INTO customers (name, email) VALUES ("Harry Potter", "harry@potter.com");
-INSERT INTO customers (name, email) VALUES ("Captain Awesome", "captain@awesome.com");
+INSERT INTO customers (id, name, email) VALUES (id_seq.NEXTVAL, 'Doctor Who', 'doctorwho@timelords.com');
+INSERT INTO customers (id, name, email) VALUES (id_seq.NEXTVAL, 'Harry Potter', 'harry@potter.com');
+INSERT INTO customers (id, name, email) VALUES (id_seq.NEXTVAL, 'Captain Awesome', 'captain@awesome.com');
 
 CREATE TABLE orders (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INTEGER PRIMARY KEY,
     customer_id INTEGER,
-    item TEXT,
+    item VARCHAR2(50),
     price REAL);
 
 INSERT INTO orders (customer_id, item, price)
-    VALUES (1, "Sonic Screwdriver", 1000.00);
+    VALUES (1, 'Sonic Screwdriver', 1000.00);
 INSERT INTO orders (customer_id, item, price)
-    VALUES (2, "High Quality Broomstick", 40.00);
+    VALUES (2, 'High Quality Broomstick', 40.00);
 INSERT INTO orders (customer_id, item, price)
-    VALUES (1, "TARDIS", 1000000.00);
+    VALUES (1, 'TARDIS', 1000000.00);
     
 SELECT customers.name, item, price from customers inner join orders on customers.id=orders.customer_id;
     
@@ -32,3 +34,7 @@ SELECT customers.name, customers.email, SUM(orders.price) AS SUM FROM customers
     ON customers.id = orders.customer_id
     GROUP BY customers.name
     ORDER BY SUM DESC;
+                   
+DROP SEQUENCE id_seq;
+DROP TABLE customers;
+DROP TABLE orders;
