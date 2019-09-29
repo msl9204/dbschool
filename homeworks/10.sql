664e650f677d270b86e4ca65bbf0a3f87c485c56
msl9204 <52311764+msl9204@users.noreply.github.com>
2019-09-30 07:25:21 +0900

    Update 10.sql

--- homeworks/10.sql
+++ homeworks/10.sql
@@ -5,21 +5,21 @@ CREATE TABLE movies (id INTEGER PRIMARY KEY,
     released INTEGER,
     sequel_id INTEGER);
 
-INSERT INTO movies 
+INSERT INTO movies (id, title, released, sequel_id) 
     VALUES (id_seq, 1, 'Harry Potter and the Philosopher''s Stone', 2001, 2);
-INSERT INTO movies 
+INSERT INTO movies (id, title, released, sequel_id) 
     VALUES (id_seq, 2, 'Harry Potter and the Chamber of Secrets', 2002, 3);
-INSERT INTO movies 
+INSERT INTO movies (id, title, released, sequel_id) 
     VALUES (id_seq, 3, 'Harry Potter and the Prisoner of Azkaban', 2004, 4);
-INSERT INTO movies 
+INSERT INTO movies (id, title, released, sequel_id) 
     VALUES (id_seq, 4, 'Harry Potter and the Goblet of Fire', 2005, 5);
-INSERT INTO movies 
+INSERT INTO movies (id, title, released, sequel_id) 
     VALUES (id_seq, 5, 'Harry Potter and the Order of the Phoenix ', 2007, 6);
-INSERT INTO movies 
+INSERT INTO movies (id, title, released, sequel_id) 
     VALUES (id_seq, 6, 'Harry Potter and the Half-Blood Prince', 2009, 7);
-INSERT INTO movies 
+INSERT INTO movies (id, title, released, sequel_id) 
     VALUES (id_seq, 7, 'Harry Potter and the Deathly Hallows – Part 1', 2010, 8);
-INSERT INTO movies 
+INSERT INTO movies (id, title, released, sequel_id) 
     VALUES (id_seq, 8, 'Harry Potter and the Deathly Hallows – Part 2', 2011, NULL);
 
 SELECT movies.title, sequel.title AS Sequel_title FROM movies
