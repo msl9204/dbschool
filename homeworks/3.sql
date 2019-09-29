65965aa6b4f2447c8de0dfe94618d60d33d20041
msl9204 <52311764+msl9204@users.noreply.github.com>
2019-09-30 06:51:13 +0900

    Update 3.sql

--- homeworks/3.sql
+++ homeworks/3.sql
@@ -1,8 +1,10 @@
 CREATE TABLE todo_list (id INTEGER PRIMARY KEY, item VARCHAR2(50), minutes INTEGER);
 
-INSERT INTO todo_list VALUES (1, "Wash the dishes", 15);
-INSERT INTO todo_list VALUES (2, "vacuuming", 20);
-INSERT INTO todo_list VALUES (3, "Learn some stuff on KA", 30);
-INSERT INTO todo_list VALUES (4, "Sleeping", 120);
+INSERT INTO todo_list VALUES (1, 'Wash the dishes', 15);
+INSERT INTO todo_list VALUES (2, 'vacuuming', 20);
+INSERT INTO todo_list VALUES (3, 'Learn some stuff on KA', 30);
+INSERT INTO todo_list VALUES (4, 'Sleeping', 120);
 
-SELECT SUM(minutes) from todo_list
+SELECT SUM(minutes) from todo_list;
+
+DROP TABLE todo_list;
