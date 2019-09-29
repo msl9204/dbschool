36313d3908f6137f6d3a454e4520489ff3c952c9
msl9204 <52311764+msl9204@users.noreply.github.com>
2019-09-30 06:59:19 +0900

    Update 5.sql

--- homeworks/5.sql
+++ homeworks/5.sql
@@ -1,65 +1,78 @@
+
+
+CREATE SEQUENCE tmp_seq START WITH 0 INCREMENT BY 1 MINVALUE 0 MAXVALUE 100 CYCLE NOCACHE;
+CREATE SEQUENCE tmp_seq2 START WITH 0 INCREMENT BY 1 MINVALUE 0 MAXVALUE 100 CYCLE NOCACHE;
+
+
+
 CREATE TABLE artists (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    country TEXT,
-    genre TEXT);
+    id number(20) PRIMARY KEY,
+    name VARCHAR2(50),
+    country VARCHAR2(50),
+    genre VARCHAR2(50));
 
-INSERT INTO artists (name, country, genre)
-    VALUES ("Taylor Swift", "US", "Pop");
-INSERT INTO artists (name, country, genre)
-    VALUES ("Led Zeppelin", "US", "Hard rock");
-INSERT INTO artists (name, country, genre)
-    VALUES ("ABBA", "Sweden", "Disco");
-INSERT INTO artists (name, country, genre)
-    VALUES ("Queen", "UK", "Rock");
-INSERT INTO artists (name, country, genre)
-    VALUES ("Celine Dion", "Canada", "Pop");
-INSERT INTO artists (name, country, genre)
-    VALUES ("Meatloaf", "US", "Hard rock");
-INSERT INTO artists (name, country, genre)
-    VALUES ("Garth Brooks", "US", "Country");
-INSERT INTO artists (name, country, genre)
-    VALUES ("Shania Twain", "Canada", "Country");
-INSERT INTO artists (name, country, genre)
-    VALUES ("Rihanna", "US", "Pop");
-INSERT INTO artists (name, country, genre)
-    VALUES ("Guns N' Roses", "US", "Hard rock");
-INSERT INTO artists (name, country, genre)
-    VALUES ("Gloria Estefan", "US", "Pop");
-INSERT INTO artists (name, country, genre)
-    VALUES ("Bob Marley", "Jamaica", "Reggae");
+INSERT INTO artists (id, name, country, genre)
+    VALUES (tmp_seq.NEXTVAL, 'Taylor Swift', 'US', 'Pop');
+INSERT INTO artists (id, name, country, genre)
+    VALUES (tmp_seq.NEXTVAL, 'Led Zeppelin', 'US', 'Hard rock');
+INSERT INTO artists (id, name, country, genre)
+    VALUES (tmp_seq.NEXTVAL, 'ABBA', 'Sweden', 'Disco');
+INSERT INTO artists (id, name, country, genre)
+    VALUES (tmp_seq.NEXTVAL, 'Queen', 'UK', 'Rock');
+INSERT INTO artists (id, name, country, genre)
+    VALUES (tmp_seq.NEXTVAL, 'Celine Dion', 'Canada', 'Pop');
+INSERT INTO artists (id, name, country, genre)
+    VALUES (tmp_seq.NEXTVAL, 'Meatloaf', 'US', 'Hard rock');
+INSERT INTO artists (id, name, country, genre)
+    VALUES (tmp_seq.NEXTVAL, 'Garth Brooks', 'US', 'Country');
+INSERT INTO artists (id, name, country, genre)
+    VALUES (tmp_seq.NEXTVAL, 'Shania Twain', 'Canada', 'Country');
+INSERT INTO artists (id, name, country, genre)
+    VALUES (tmp_seq.NEXTVAL, 'Rihanna', 'US', 'Pop');
+INSERT INTO artists (id, name, country, genre)
+    VALUES (tmp_seq.NEXTVAL, 'Guns N'' Roses', 'US', 'Hard rock');
+INSERT INTO artists (id, name, country, genre)
+    VALUES (tmp_seq.NEXTVAL, 'Gloria Estefan', 'US', 'Pop');
+INSERT INTO artists (id, name, country, genre)
+    VALUES (tmp_seq.NEXTVAL, 'Bob Marley', 'Jamaica', 'Reggae');
 
+                   
+DROP TABLE songs;
+                   
 CREATE TABLE songs (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    artist TEXT,
-    title TEXT);
+    id number(20) PRIMARY KEY,
+    artist VARCHAR2(50),
+    title VARCHAR2(50));
 
-INSERT INTO songs (artist, title)
-    VALUES ("Taylor Swift", "Shake it off");
-INSERT INTO songs (artist, title)
-    VALUES ("Rihanna", "Stay");
-INSERT INTO songs (artist, title)
-    VALUES ("Celine Dion", "My heart will go on");
-INSERT INTO songs (artist, title)
-    VALUES ("Celine Dion", "A new day has come");
-INSERT INTO songs (artist, title)
-    VALUES ("Shania Twain", "Party for two");
-INSERT INTO songs (artist, title)
-    VALUES ("Gloria Estefan", "Conga");
-INSERT INTO songs (artist, title)
-    VALUES ("Led Zeppelin", "Stairway to heaven");
-INSERT INTO songs (artist, title)
-    VALUES ("ABBA", "Mamma mia");
-INSERT INTO songs (artist, title)
-    VALUES ("Queen", "Bicycle Race");
-INSERT INTO songs (artist, title)
-    VALUES ("Queen", "Bohemian Rhapsody");
-INSERT INTO songs (artist, title)
-    VALUES ("Guns N' Roses", "Don't cry");
+INSERT INTO songs (id, artist, title)
+    VALUES (tmp_seq2.NEXTVAL, 'Taylor Swift', 'Shake it off');
+INSERT INTO songs (id, artist, title)
+    VALUES (tmp_seq2.NEXTVAL, 'Rihanna', 'Stay');
+INSERT INTO songs (id, artist, title)
+    VALUES (tmp_seq2.NEXTVAL, 'Celine Dion', 'My heart will go on');
+INSERT INTO songs (id, artist, title)
+    VALUES (tmp_seq2.NEXTVAL, 'Celine Dion', 'A new day has come');
+INSERT INTO songs (id, artist, title)
+    VALUES (tmp_seq2.NEXTVAL, 'Shania Twain', 'Party for two');
+INSERT INTO songs (id, artist, title)
+    VALUES (tmp_seq2.NEXTVAL, 'Gloria Estefan', 'Conga');
+INSERT INTO songs (id, artist, title)
+    VALUES (tmp_seq2.NEXTVAL, 'Led Zeppelin', 'Stairway to heaven');
+INSERT INTO songs (id, artist, title)
+    VALUES (tmp_seq2.NEXTVAL, 'ABBA', 'Mamma mia');
+INSERT INTO songs (id, artist, title)
+    VALUES (tmp_seq2.NEXTVAL, 'Queen', 'Bicycle Race');
+INSERT INTO songs (id, artist, title)
+    VALUES (tmp_seq2.NEXTVAL, 'Queen', 'Bohemian Rhapsody');
+INSERT INTO songs (id, artist, title)
+    VALUES (tmp_seq2.NEXTVAL, 'Guns N'' Roses', 'Don''t cry');
     
 SELECT title FROM songs WHERE artist = 'Queen';
 SELECT name FROM artists WHERE genre = 'Pop';
 SELECT title FROM songs WHERE artist IN (SELECT name FROM artists WHERE genre = 'Pop');
 
+DROP SEQUENCE tmp_seq;
+DROP SEQUENCE tmp_seq2;
 
-
+DROP TABLE artists;
+DROP TABLE songs;
