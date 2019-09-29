1c15b73c9b211c27d8f954a32f321a8156cc9735
msl9204 <52311764+msl9204@users.noreply.github.com>
2019-09-30 08:24:39 +0900

    Update 9.sql

--- homeworks/9.sql
+++ homeworks/9.sql
@@ -28,13 +28,12 @@ SELECT customers.name, customers.email, orders.item, orders.price FROM customers
     LEFT OUTER JOIN orders
     ON customers.id = orders.customer_id;
     
-SELECT customers.name, customers.email, SUM(orders.price) AS SUM FROM customers
-    LEFT OUTER JOIN orders
-    ON (SELECT customers.name, customers.email, SUM(orders.price) AS SUM FROM customers
-    LEFT OUTER JOIN orders
-    ON customers.id = orders.customer_id
-    GROUP BY customers.name)                   
-    ORDER BY SUM DESC;
+SELECT customers.name, customers.email, SUM(orders.price) AS SUM FROM customers ,
+        (SELECT customers.name, customers.email, SUM(orders.price) AS SUM FROM customers
+         LEFT OUTER JOIN orders
+         ON customers.id = orders.customer_id
+         GROUP BY customers.name)                   
+ORDER BY SUM DESC;
                    
 DROP SEQUENCE id_seq;
 DROP SEQUENCE id_seq2;
