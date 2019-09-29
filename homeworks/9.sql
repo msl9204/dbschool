e60216433a885c130e730e4084565b2dffe72c7f
msl9204 <52311764+msl9204@users.noreply.github.com>
2019-09-30 08:08:08 +0900

    Update 9.sql

--- homeworks/9.sql
+++ homeworks/9.sql
@@ -28,7 +28,7 @@ SELECT customers.name, customers.email, orders.item, orders.price FROM customers
     LEFT OUTER JOIN orders
     ON customers.id = orders.customer_id;
     
-SELECT customers.name, customers.email, SUM(orders.price) AS SUM FROM customers
+SELECT customers.name, customers.email, SUM(orders.price) FROM customers
     LEFT OUTER JOIN orders
     ON customers.id = orders.customer_id
     GROUP BY customers.name
