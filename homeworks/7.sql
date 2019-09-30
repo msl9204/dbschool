71227c724a5f6ebe096888832a58c22662a533fb
msl9204 <52311764+msl9204@users.noreply.github.com>
2019-09-30 13:31:20 +0900

    Update 7.sql

--- homeworks/7.sql
+++ homeworks/7.sql
@@ -7,31 +7,30 @@ CREATE TABLE student_grades (
     number_grade INTEGER,
     fraction_completed REAL);
     
-INSERT INTO student_grades (id, name, number_grade, fraction_completed)
+INSERT INTO student_grades (id_seq, name, number_grade, fraction_completed)
     VALUES (id_seq.NEXTVAL, 'Winston', 90, 0.805);
-INSERT INTO student_grades (id, name, number_grade, fraction_completed)
+INSERT INTO student_grades (id_seq, name, number_grade, fraction_completed)
     VALUES (id_seq.NEXTVAL, 'Winnefer', 95, 0.901);
-INSERT INTO student_grades (id, name, number_grade, fraction_completed)
+INSERT INTO student_grades (id_seq, name, number_grade, fraction_completed)
     VALUES (id_seq.NEXTVAL, 'Winsteen', 85, 0.906);
-INSERT INTO student_grades (id, name, number_grade, fraction_completed)
+INSERT INTO student_grades (id_seq, name, number_grade, fraction_completed)
     VALUES (id_seq.NEXTVAL, 'Wincifer', 66, 0.7054);
-INSERT INTO student_grades (id, name, number_grade, fraction_completed)
+INSERT INTO student_grades (id_seq, name, number_grade, fraction_completed)
     VALUES (id_seq.NEXTVAL, 'Winster', 76, 0.5013);
-INSERT INTO student_grades (id, name, number_grade, fraction_completed)
+INSERT INTO student_grades (id_seq, name, number_grade, fraction_completed)
     VALUES (id_seq.NEXTVAL, 'Winstonia', 82, 0.9045);
 
 SELECT name, number_grade, ROUND(fraction_completed*100) AS percent_completed FROM student_grades;
 
-SELECT COUNT(*)
-FROM (SELECT COUNT(*), 
+SELECT COUNT(*),
     CASE
         WHEN number_grade > 90 THEN 'A'
         WHEN number_grade > 80 THEN 'B'
         WHEN number_grade > 70 THEN 'C'
         ELSE 'F'
     END AS letter_grade
-    FROM student_grades GROUP BY letter_grade)
-    FROM student_grades;
+FROM student_grades
+GROUP BY letter_grade;
 
 DROP SEQUENCE id_seq;
 DROP TABLE student_grades;
