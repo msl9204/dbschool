07ec902a0d2ace135616e99ebfe2b0ea6b7c4408
msl9204 <52311764+msl9204@users.noreply.github.com>
2019-12-03 15:07:35 +0900

    Update test5.sql

--- test5.sql
+++ test5.sql
@@ -1,23 +1,23 @@
 set serveroutput on
 set verify off
 variable e_name varchar2(30)
-accept emp_no prompt "Ã£°íÀÚÇÏ´Â Á¾¾÷¿ø ¹øÈ£´Â?"
+accept emp_no prompt "employee number??"
 declare
        v_empno number(4);
        v_ename emp.ename%type;
 
        v_cnt number(5);
 begin
-      /*select.. into.. ¹®Àå Å×½ºÆ®*/
+      /*select.. into.. ë¬¸ì¥ í…ŒìŠ¤íŠ¸*/
       select empno, ename
       into v_empno, v_ename
       from emp
       where empno = &emp_no;
       :e_name := v_ename;
 
-      dbms_output.put_line('°á°ú=> ' ||'employee#: ' || v_empno || ', ' || 'employee name: '|| v_ename);
+      dbms_output.put_line('ê²°ê³¼=> ' ||'employee#: ' || v_empno || ', ' || 'employee name: '|| v_ename);
 
-      /*update¹®Àå ¹× %rowcount Å×½ºÆ®*/
+      /*updateë¬¸ì¥ ë° %rowcount í…ŒìŠ¤íŠ¸*/
       select count(*)
       into v_cnt
       from emp
@@ -39,8 +39,8 @@ begin
 
       exception
 	when NO_DATA_FOUND THEN
-		dbms_output.put_line('Á¾¾÷¿ø ¹øÈ£' || &emp_no || '´Â Ã£À» ¼ö°¡ ¾ø½À´Ï´Ù');
-		:e_name := 'ÇØ´çµÇ´Â Á¾¾÷¿ø¾øÀ½';
+		dbms_output.put_line('empno : ' || &emp_no || 'Not Found');
+		:e_name := 'Not Found';
 
 end;
 /
