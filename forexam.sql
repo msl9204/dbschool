684d59e4189481f9822696a4c149e6452fcc7a74
msl9204 <52311764+msl9204@users.noreply.github.com>
2019-12-17 12:25:56 +0900

    Update forexam.sql

--- forexam.sql
+++ forexam.sql
@@ -4,7 +4,7 @@ set serveroutput ON;
 DECLARE
     cnt integer;
 BEGIN
-    cnt := cnt + 1
+    cnt := cnt + 1;
     if cnt is null then
         dbms_output.put_line('결과는 : cnt는 NULL입니다.');
     end if;
